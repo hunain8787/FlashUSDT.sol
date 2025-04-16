03ca26ef001418adcfde2d9ebc8dcdb97bf9c398
hunain8787 <Hunainwahla1122@gmail.com>
2025-04-16 14:07:11 +0500

    Update and rename README.md to FlashUSDT.sol

---
+++ FlashUSDT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract FlashUSDT is ERC20, Ownable {
+    constructor() ERC20("Flash USDT", "fUSDT") {
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6; // Like real USDT
+    }
+}
